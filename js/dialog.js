9652bce53144d48abc3caaaa09d994669085f7e3
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-01-11 21:41:39 -0600

    Rename to dialog.js

---
+++ js/dialog.js
@@ -0,0 +1,8 @@
+function openDialog(dialog) {
+    dialog.showModal();
+    dialog.scrollTo({ top: 0 })
+}
+
+function closeDialog(dialog) {
+    dialog.close();
+}
--- js/services.js
+++
@@ -1,7 +0,0 @@
-function openService(dialog) {
-    dialog.showModal();
-}
-
-function closeService(dialog) {
-    dialog.close();
-}
